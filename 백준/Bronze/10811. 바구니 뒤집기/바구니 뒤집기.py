08e0ec706e027f76cf03c6ca48a9efa35781b2b8
jbj338033 <82754696+jbj338033@users.noreply.github.com>
2024-01-01 23:27:11 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,18 +1,12 @@
-from sys import stdin
-
 n, m = map(int, input().split())
 
-l = [i for i in range(1, n +1)]
+l = [i + 1 for i in range(n)]
 
 for _ in range(m):
-    i, j = map(int, stdin.readline().split())
+    i, j = map(int, input().split())
 
-    for a in range((j - i) // 2 + 1):
-        b = i + a - 1
-        c = j - a - 1
+    # [::-1]
+    l[i - 1:j] = l[i - 1:j][::-1]
 
-        tmp = l[b]
-        l[b] = l[c]
-        l[c] = tmp
-
-print(" ".join(map(str, l)))
+# *l
+print(*l)
