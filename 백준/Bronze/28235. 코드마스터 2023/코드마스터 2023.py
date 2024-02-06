7a259a716ac98a089215a4a281f92984da392a7a
jbj338033 <82754696+jbj338033@users.noreply.github.com>
2024-02-06 13:34:39 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+n = input()
+
+if n == "SONGDO":
+    print("HIGHSCHOOL")
+elif n == "CODE":
+    print("MASTER")
+elif n == "2023":
+    print("0611")
+elif n == "ALGORITHM":
+    print("CONTEST")
