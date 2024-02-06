e0e697fc7473c1bd417a9c66a3b15fe01815f73e
jbj338033 <82754696+jbj338033@users.noreply.github.com>
2024-02-06 12:17:34 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,9 +1,5 @@
 n, k = map(int, input().split())
-arr = []
-
-for i in range(1, n+1):
-    if n % i == 0:
-        arr.append(i)
+arr = [i for i in range(1, n+1) if n % i == 0]
 
 if len(arr) < k:
     print(0)
