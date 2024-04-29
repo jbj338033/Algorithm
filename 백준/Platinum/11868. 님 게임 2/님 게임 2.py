64c49174ee90057a772d4ffc1c0daeb48e3c3959
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-04-29 16:45:35 +0900

    [Platinum IV] Title: 님 게임 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/11868. 님 게임 2/님 게임 2.py
+++ 백준/Platinum/11868. 님 게임 2/님 게임 2.py
@@ -1,13 +1,8 @@
 n=int(input())
 p=list(map(int,input().split()))
 
-if n==1:
-    print('koosaga')
+if n==1:print('koosaga')
 else:
     a=0
-    for i in p:
-        a^=i
-    if a==0:
-        print('cubelover')
-    else:
-        print('koosaga')
+    for i in p:a^=i
+    print('koosaga' if a else 'cubelover')
