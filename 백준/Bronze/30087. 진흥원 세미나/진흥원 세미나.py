ae2ed7c06ba880be142661456426afe8ea129b7d
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-05-12 21:40:44 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,2 @@
+for _ in range(int(input())):
+    print({'Algorithm':204,'DataAnalysis':207,'ArtificialIntelligence':302,'CyberSecurity':'B101','Network':'303','Startup':501,'TestStrategy':105}[input()])
