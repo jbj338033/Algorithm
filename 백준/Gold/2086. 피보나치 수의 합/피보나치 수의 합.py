5ad720dcd2e42250e3dcd471b321c83915cd5f47
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-06-04 20:54:55 +0900

    [Gold I] Title: 피보나치 수의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2086. 피보나치 수의 합/피보나치 수의 합.py
@@ -0,0 +1,13 @@
+import sys
+d={1:1,2:1,3:2,4:3,5:5}
+def f(n):
+  if d.get(n):
+    return d[n]
+  else:
+    if n%2 == 1:
+      d[n]=(f(n//2)**2+f(n//2+1)**2)%1000000000
+    else:
+      d[n]=(f(n+1)-f(n-1))%1000000000
+    return d[n]
+a,b=map(int,sys.stdin.readline().split())
+print((f(b+2)-f(a+1))%1000000000)
