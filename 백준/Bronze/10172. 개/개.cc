9b0eb40699c2116cb1db5e73cbeb1422a9ebd78f
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-07-01 23:29:09 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    cout << "|\\_/|" << "\n";
+    cout << "|q p|   /}" << "\n";
+    cout << "( 0 )\"\"\"\\" << "\n";
+    cout << "|\"^\"`    |" << "\n";
+    cout << "||_/=\\\\__|";
+
+    return 0;
+}
