b2fb0a4d346edf5f5b04941e45f0ff5f0174e3a3
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-07-01 23:27:32 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( ')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|";
+
+    return 0;
+}
