a7d415278b10aab643ae8fb84469b9ca7438e7b9
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-07-02 08:58:19 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
