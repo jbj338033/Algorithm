8d720747d3ad5c53f651620250d44b96aa7234d5
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-09-04 20:18:03 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,8 @@
-def gcd(a,b):
-    if a%b==0:return b
-    elif b==0:return a
-    else:return gcd(b,a%b)
-def lcm(a,b):
-    return a*b//gcd(a,b)
+def gcd(n,m):
+    while m!=0:n,m=m,n%m
+    return abs(n)
+def lcm(n,m):
+    return int((n*m)/gcd(n,m))
 a,b=map(int,input().split())
 print(gcd(a,b))
 print(lcm(a,b))
