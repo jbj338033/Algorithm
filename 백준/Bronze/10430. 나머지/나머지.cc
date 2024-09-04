a0b1ac60ebab748346d997a6072118b3778ee3f1
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-09-04 19:22:07 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.cc
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -1,16 +1,12 @@
 #include <iostream>
 
-using namespace std;
+using namespace std;;
 
 int main() {
     int a, b, c;
-
     cin >> a >> b >> c;
-
     cout << (a + b) % c << "\n";
     cout << ((a % c) + (b % c)) % c << "\n";
     cout << (a * b) % c << "\n";
     cout << ((a % c) * (b % c)) % c << "\n";
-
-    return 0;
 }
