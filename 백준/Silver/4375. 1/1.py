2ea7d7418bfaa43a07b9e9b5a316562e61017d0e
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-09-04 19:28:39 +0900

    [Silver III] Title: 1, Time: 1588 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.py
+++ 백준/Silver/4375. 1/1.py
@@ -1,10 +1,10 @@
 while 1:
     try:
         n=int(input())
-        i='1'
-        c=1
-        while int(i)%n!=0:
-            i+='1'
-            c+=1
-        print(c)
+        i=1
+        while 1:
+            if int('1'*i)%n==0:
+                print(i)
+                break
+            i+=1
     except:break
