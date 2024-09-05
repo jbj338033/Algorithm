5dbd430911cb08ffa4a75fcfa4afbc4870ad2700
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-09-05 15:42:17 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,14 +1,20 @@
 import sys
-
-queue = []
-
+from collections import deque
+input=sys.stdin.readline
+q=deque()
 for _ in range(int(input())):
-    s=sys.stdin.readline().split()
+    s=input().split()
     c=s[0]
 
-    if c == "push":queue.append(int(s[1]))
-    elif c == "pop":print(queue.pop(0) if queue else -1)
-    elif c == "size":print(len(queue))
-    elif c == "empty":print(int(not bool(queue)))
-    elif c == "front":print(queue[0] if queue else -1)
-    else: print(queue[-1] if queue else -1)
+    if c=='push':
+        q.appendleft(int(s[1]))
+    elif c=='pop':
+        print(q.pop() if q else -1)
+    elif c=='size':
+        print(len(q))
+    elif c=='empty':
+        print(0 if q else 1)
+    elif c=='front':
+        print(q[-1] if q else -1)
+    else:
+        print(q[0] if q else -1)
