b8f50826c7f7e66de438de0fbe8bb8926e3f5be4
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-09-05 20:25:04 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,25 +1,23 @@
-from sys import stdin
+import sys
 from collections import deque
-
-d=deque()
-
+input=sys.stdin.readline
+q=deque()
 for _ in range(int(input())):
-    s=stdin.readline().split()
+    s=input().split()
     c=s[0]
-
-    if c == "push_front":
-        d.appendleft(s[1])
-    elif c == "push_back":
-        d.append(s[1])
-    elif c == "pop_front":
-        print(d.popleft() if d else -1)
-    elif c == "pop_back":
-        print(d.pop() if d else -1)
-    elif c == "size":
-        print(len(d))
-    elif c == "empty":
-        print(int(not bool(d)))
-    elif c == "front":
-        print(d[0] if d else -1)
-    elif c == "back":
-        print(d[-1] if d else -1)
+    if c=='push_front':
+        q.appendleft(int(s[1]))
+    elif c=='push_back':
+        q.append(int(s[1]))
+    elif c=='pop_front':
+        print(q.popleft() if q else -1)
+    elif c=='pop_back':
+        print(q.pop() if q else -1)
+    elif c=='size':
+        print(len(q))
+    elif c=='empty':
+        print(0 if q else 1)
+    elif c=='front':
+        print(q[0] if q else -1)
+    else:
+        print(q[-1] if q else -1)
